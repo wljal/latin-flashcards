4aa5fe9a8c42f0300cf51f056c5b769d617e8dd0
wljal <wljall@proton.me>
2025-05-01 22:29:18 +0000

    redo again

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ function updateCsvs() {
 $(document).on('click', '#next', function () { selectedWord++; if (selectedWord > latinWords.length) selectedWord = latinWords.length; flipped = false; updateFlashcard(); });
 $(document).on('click', '#prev', function () { selectedWord--; if (selectedWord < 0) selectedWord = 0; flipped = false; updateFlashcard(); });
 $(document).on('click', '.flashcard', function () { flipped = !flipped; updateFlashcard(); });
-$(document).on('click', '.shuffle', function () { shuffleArray(latinWords); updateFlashcard(); });
+$(document).on('click', '.shuffle', function () { shuffleArray(latinWords); flipped = false; updateFlashcard(); });
 $(document).on('click', '.delete', function () { latinWords.splice(selectedWord, 1); englishDefinitions.slice(selectedWord, 1); updateFlashcard(); });
 
 $(document).on('click', '#load-chapters', function () { updateCsvs(); });
